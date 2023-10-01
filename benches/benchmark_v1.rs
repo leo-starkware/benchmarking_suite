b38b90a764dca2bd20b5f4c5e480f12bd31ac7dd
amanusk <amanusk@pm.me>
2023-10-01 15:33:43 +0300

    Fix async criterion :yay:

--- benches/benchmark_v1.rs
+++ benches/benchmark_v1.rs
@@ -1,21 +1,19 @@
-use benchmarking_suite::{get_block_with_tx_hashes, get_block_with_txs, BenchedProvider};
+use benchmarking_suite::BenchedProvider;
 use starknet::core::types::BlockId;
+use starknet::providers::Provider;
+
+use tokio::runtime::Runtime;
 
 use std::iter;
-// use bench_suite_v3::{BenchInputer, BenchTrait, request, provider};
 
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
-// use std::env;
 
 const BLAST_ADDRESS: &str = "https://starknet-mainnet.public.blastapi.io";
 const LOCAL_ADDRESS: &str = "http://192.168.1.45:1235";
 
-// https://starknet-mainnet.public.blastapi.io
-// https://starknet-mainnet.infura.io/v3/aa3a17de2faa41c39083706ba85ac1c6
-
 pub fn criterion_request(c: &mut Criterion) {
-    // let args: Vec<String> = env::args().collect();
-    // println!("{}", args[0].as_str());
+    let rt = tokio::runtime::Runtime::new().unwrap();
+
     let mut group = c.benchmark_group("get_block");
     let blast_provider = BenchedProvider::new(BLAST_ADDRESS);
     let local_provider = BenchedProvider::new(LOCAL_ADDRESS);
@@ -26,12 +24,13 @@ pub fn criterion_request(c: &mut Criterion) {
             BenchmarkId::from_parameter(provider.url.as_str()),
             &provider,
             |b, provider| {
-                b.iter(|| get_block_with_tx_hashes(&provider.provider, BlockId::Number(20000)))
+                b.to_async(&rt).iter(|| {
+                    provider
+                        .provider
+                        .get_block_with_tx_hashes(BlockId::Number(0))
+                })
             },
         );
-        // c.bench_function("bench", |b| {
-        //     b.iter(|| get_block_with_txs(&provider, BlockId::Number(20000)))
-        // });
     }
 }
 
