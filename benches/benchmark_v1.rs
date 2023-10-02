f068890a4dbb3d04d36680438bc417beb4b2e17c
Leo <leonardo@Leonardos-MacBook-Pro.local>
2023-10-02 15:49:52 +0300

    json reader and 4 RPC calls hardcoded

--- benches/benchmark_v1.rs
+++ benches/benchmark_v1.rs
@@ -3,24 +3,28 @@ use benchmarking_suite::BenchedProvider;
 use starknet::{core::types::BlockId, providers::Provider};
 
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+mod config_reader;
+use config_reader::RawInputs;
 
-const BLAST_ADDRESS: &str = "https://starknet-mainnet.public.blastapi.io";
-const LOCAL_ADDRESS: &str = "http://192.168.1.45:1235";
+// const BLAST_ADDRESS: &str = "https://starknet-mainnet.public.blastapi.io";
+// const LOCAL_ADDRESS: &str = "http://192.168.1.45:1235";
+const PATH: &str = "./config/config.json";
 
 pub fn bench_providers(c: &mut Criterion) {
     let rt = tokio::runtime::Runtime::new().unwrap();
-
     let mut group = c.benchmark_group("providers");
-
-    let blast_provider = BenchedProvider::new(BLAST_ADDRESS);
-    let local_provider = BenchedProvider::new(LOCAL_ADDRESS);
-    let providers = vec![blast_provider, local_provider];
+    let inputs = RawInputs::new_from_json(PATH);
     let blocks = vec!["latest", "pending"];
 
-    for provider in &providers {
+    // let blast_provider = BenchedProvider::new(BLAST_ADDRESS);
+    // let local_provider = BenchedProvider::new(LOCAL_ADDRESS);
+    // let providers = vec![blast_provider, local_provider];
+
+    for url in inputs.urls {
+        let provider = BenchedProvider::new(url.as_str());
         group
             .bench_with_input(
-                BenchmarkId::new("get_block/", provider.url.as_str()),
+                BenchmarkId::new("get_block_with_tx_hashes/", provider.url.as_str()),
                 &provider,
                 |b, provider| {
                     b.to_async(&rt).iter(|| {
@@ -31,9 +35,7 @@ pub fn bench_providers(c: &mut Criterion) {
                 },
             )
             .sample_size(10);
-    }
 
-    for provider in &providers {
         group
             .bench_with_input(
                 BenchmarkId::new("pending_transactions/", provider.url.as_str()),
@@ -44,6 +46,35 @@ pub fn bench_providers(c: &mut Criterion) {
                 },
             )
             .sample_size(10);
+
+        group
+        .bench_with_input(
+            BenchmarkId::new("block_number/", provider.url.as_str()),
+            &provider,
+            |b, provider| {
+                b.to_async(&rt).iter(|| {
+                    provider
+                        .provider
+                        .block_number()
+                })
+            },
+        )
+        .sample_size(10);
+
+        group
+        .bench_with_input(
+            BenchmarkId::new("get_state_update/", provider.url.as_str()),
+            &provider,
+            |b, provider| {
+                b.to_async(&rt).iter(|| {
+                    provider
+                        .provider
+                        .get_state_update(BlockId::Number(10))
+                })
+            },
+        )
+        .sample_size(10);
+        
     }
 }
 
@@ -51,8 +82,8 @@ pub fn bench_blocks(c: &mut Criterion) {
     let rt = tokio::runtime::Runtime::new().unwrap();
 
     let mut group = c.benchmark_group("blocks");
-
-    let local_provider = BenchedProvider::new(LOCAL_ADDRESS);
+    let local_address: &str = "http://192.168.1.45:1235";
+    let local_provider = BenchedProvider::new(local_address);
     let blocks = vec!["latest", "pending"];
 
     for block in &blocks {
