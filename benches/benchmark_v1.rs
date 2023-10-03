0b4e7f8fab0ea30dd4db88d245904a12bc3d1aee
Leo <leonardo@Leonardos-MacBook-Pro.local>
2023-10-03 12:04:57 +0300

    created new type BenchRunner that encapsulates the benching of methods. The big pattern matching goes inside one of its methods

--- benches/benchmark_v1.rs
+++ benches/benchmark_v1.rs
@@ -1,101 +1,21 @@
 use benchmarking_suite::utils::parse_block_id;
-use benchmarking_suite::BenchedProvider;
-use starknet::{core::types::BlockId, providers::Provider};
+use benchmarking_suite::{BenchedProvider, BenchRunner};
+use starknet::providers::Provider;
 
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
-mod config_reader;
-use config_reader::RawInputs;
 
-// const BLAST_ADDRESS: &str = "https://starknet-mainnet.public.blastapi.io";
-// const LOCAL_ADDRESS: &str = "http://192.168.1.45:1235";
 const PATH: &str = "./config/config.json";
 
 pub fn bench_providers(c: &mut Criterion) {
     let rt = tokio::runtime::Runtime::new().unwrap();
     let mut group = c.benchmark_group("providers");
-    let inputs = RawInputs::new_from_json(PATH);
-    let blocks = vec!["latest", "pending"];
+    let bench_runner = BenchRunner::new_from_json(PATH);
 
-    // let blast_provider = BenchedProvider::new(BLAST_ADDRESS);
-    // let local_provider = BenchedProvider::new(LOCAL_ADDRESS);
-    // let providers = vec![blast_provider, local_provider];
-
-    for url in inputs.urls {
+    for url in bench_runner.inputs.urls.iter() {
         let provider = BenchedProvider::new(url.as_str());
-        group
-            .bench_with_input(
-                BenchmarkId::new("get_block_with_tx_hashes/", provider.url.as_str()),
-                &provider,
-                |b, provider| {
-                    b.to_async(&rt).iter(|| {
-                        provider
-                            .provider
-                            .get_block_with_tx_hashes(BlockId::Number(0))
-                    })
-                },
-            )
-            .sample_size(10);
-
-        group
-            .bench_with_input(
-                BenchmarkId::new("pending_transactions/", provider.url.as_str()),
-                &provider,
-                |b, provider| {
-                    b.to_async(&rt)
-                        .iter(|| provider.provider.pending_transactions())
-                },
-            )
-            .sample_size(10);
-
-        group
-        .bench_with_input(
-            BenchmarkId::new("block_number/", provider.url.as_str()),
-            &provider,
-            |b, provider| {
-                b.to_async(&rt).iter(|| {
-                    provider
-                        .provider
-                        .block_number()
-                })
-            },
-        )
-        .sample_size(10);
-
-        group
-        .bench_with_input(
-            BenchmarkId::new("get_state_update/", provider.url.as_str()),
-            &provider,
-            |b, provider| {
-                b.to_async(&rt).iter(|| {
-                    provider
-                        .provider
-                        .get_state_update(BlockId::Number(10))
-                })
-            },
-        )
-        .sample_size(10);
-        
-    }
-}
-
-pub fn bench_blocks(c: &mut Criterion) {
-    let rt = tokio::runtime::Runtime::new().unwrap();
-
-    let mut group = c.benchmark_group("blocks");
-    let local_address: &str = "http://192.168.1.45:1235";
-    let local_provider = BenchedProvider::new(local_address);
-    let blocks = vec!["latest", "pending"];
-
-    for block in &blocks {
-        group
-            .bench_with_input(BenchmarkId::new("get_block/", block), &block, |b, block| {
-                b.to_async(&rt).iter(|| {
-                    local_provider
-                        .provider
-                        .get_block_with_tx_hashes(parse_block_id(block).unwrap())
-                })
-            })
-            .sample_size(10);
+        for method_name in bench_runner.inputs.methods.iter() {
+            BenchRunner::run_by_method(&mut group, &provider, method_name, &rt);
+        }
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 use starknet::providers::jsonrpc::{HttpTransport, JsonRpcClient};
-use url::Url;
+use starknet::{core::types::BlockId, providers::Provider};
 
+use url::Url;
+use tokio::runtime::Runtime;
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, BenchmarkGroup, measurement::WallTime};
+use std::fs;
+use serde::{Serialize,Deserialize};
+use serde_json;
 pub mod utils;
 
 #[derive(Debug)]
@@ -12,13 +18,79 @@ pub struct BenchedProvider {
 impl BenchedProvider {
     pub fn new(url: &str) -> Self {
         Self {
-            provider: provider(url),
+            provider: BenchedProvider::url_to_client(url),
             url: url.to_string(),
         }
     }
+
+    // Get a client from a url address
+    fn url_to_client(address: &str) -> JsonRpcClient<HttpTransport> {
+        JsonRpcClient::new(HttpTransport::new(Url::parse(address).unwrap()))
+    }
 }
 
-// Get a client from a url address
-pub fn provider(address: &str) -> JsonRpcClient<HttpTransport> {
-    JsonRpcClient::new(HttpTransport::new(Url::parse(address).unwrap()))
+
+#[derive(Serialize, Deserialize)]
+pub struct RawInputs {
+    pub urls: Vec<String>,
+    pub methods: Vec<String>,
+}
+pub struct BenchRunner {
+    pub inputs: RawInputs
+}
+impl BenchRunner {
+    pub fn new_from_json(path: &str) -> BenchRunner {
+        let contents = fs::read_to_string(path).expect("Config file not found");
+        let deserialized: RawInputs = serde_json::from_str(contents.as_str()).unwrap();
+        BenchRunner { inputs: deserialized }
+    }
+
+    pub fn run_by_method(group: &mut BenchmarkGroup<'_, WallTime>, provider: &BenchedProvider, method_name: &str, runner: &tokio::runtime::Runtime) {
+        match method_name {
+            "starknet_getStateUpdate" => {group
+                .bench_with_input(
+                    BenchmarkId::new("get_state_update", provider.url.as_str()),
+                    &provider,
+                    |b, provider| {
+                        b.to_async(runner).iter(|| {
+                            provider
+                                .provider
+                                .get_state_update(BlockId::Number(10))
+                        })
+                    },
+                )
+                .sample_size(10);},
+
+            "starknet_blockNumber" => {group
+                .bench_with_input(
+                    BenchmarkId::new("block_number", provider.url.as_str()),
+                    &provider,
+                    |b, provider| {
+                        b.to_async(runner).iter(|| {
+                            provider
+                                .provider
+                                .block_number()
+                        })
+                    },
+                )
+                .sample_size(10);},
+
+            "starknet_getBlockWithTxHashes" => {group
+                .bench_with_input(
+                    BenchmarkId::new("get_block_with_tx_hashes", provider.url.as_str()),
+                    &provider,
+                    |b, provider| {
+                        b.to_async(runner).iter(|| {
+                            provider
+                                .provider
+                                .get_block_with_tx_hashes(BlockId::Number(10))
+                        })
+                    },
+                )
+                .sample_size(10);},
+
+            "starknet_pending" => (),
+            _ => ()
+        }
+    }
 }
